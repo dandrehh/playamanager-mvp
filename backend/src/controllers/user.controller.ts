d6478d59930089e489cf77022f23ab4217d35f16
Daniel Pauvif Ortiz <dportiz127@gmail.com>
2026-02-21 16:47:40 -0300

    Fix bcrypt import to use bcrypt instead of bcryptjs

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import prisma from '../config/database';
 
 // GET /api/users - Listar usuarios (solo admin)
